5f5a54e36da178c9bce6c7d68be2ae1744565b7a
Zachary Anker <zach.anker@gmail.com>
2011-07-15 10:17:06 -0700

    Make StandardNames optional

--- lib/rets/base/core.rb
+++ lib/rets/base/core.rb
@@ -89,7 +89,7 @@ module RETS
           raise RETS::CapabilityNotFound.new("Cannot find URL for Search call")
         end
 
-        @http.request(:url => @urls[:Search], :read_timeout => args[:read_timeout], :params => {:Format => "COMPACT-DECODED", :SearchType => args[:search_type], :StandardNames => 1, :QueryType => "DMQL2", :Query => args[:query], :Class => args[:class]}) do |response|
+        @http.request(:url => @urls[:Search], :read_timeout => args[:read_timeout], :params => {:Format => "COMPACT-DECODED", :SearchType => args[:search_type], :StandardNames => (args[:standard_names] && 1 || 0), :QueryType => "DMQL2", :Query => args[:query], :Class => args[:class]}) do |response|
           stream = RETS::StreamHTTP.new(response)
 
           doc = Nokogiri::XML::SAX::Parser.new(RETS::Base::SAXSearch.new(block))
