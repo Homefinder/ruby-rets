337ef87720d656c69f3a71262e287bc4b2c29d73
Zachary Anker <zach.anker@gmail.com>
2012-01-26 00:18:53 -0800

    Documentation cleanup Fixed handling of multiple WWW-Authenticate and Set-Cookie headers Redo how authentication is handled so it's less fragile and will work with most RETS 1.x implementations Use RETS-Version when it's returned from the server for RETS-UA-Authorization, fallback on "RETS/1.7" if none is found though

--- lib/rets/http.rb
+++ lib/rets/http.rb
@@ -1,16 +1,23 @@
 require "cgi"
-require "net/https"
-require "nokogiri"
+require "net/http"
 require "digest"
 
 module RETS
   class HTTP
     ##
     # Creates a new HTTP instance which will automatically handle authenting to the RETS server.
-    def initialize(auth, user_agent=nil)
+    def initialize(args)
+      @headers = {"User-Agent" => "Ruby RETS/v#{RETS::VERSION}"}
       @request_count = 1
-      @headers = {"User-Agent" => (user_agent || "Ruby RETS/v#{RETS::VERSION}")}
-      @auth = auth
+      @config = args
+
+      if @config[:useragent] and @config[:useragent][:name]
+        @headers["User-Agent"] = @config[:useragent][:name]
+      end
+
+      if @config[:auth_mode] == :basic
+        @auth_mode = @config.delete(:auth_mode)
+      end
     end
 
     ##
@@ -18,8 +25,8 @@ module RETS
     # if the WWW-Authorization header is passed, then it will overwrite what it knows about the auth data.
     def save_digest(header)
       @request_count = 0
-      @digest = {}
 
+      @digest = {}
       header.split(",").each do |line|
         k, v = line.strip.split("=", 2)
         @digest[k] = (k != "algorithm" and k != "stale") && v[1..-2] || v
@@ -32,9 +39,9 @@ module RETS
     ##
     # Creates a HTTP digest header.
     def create_digest(method, request_uri)
-      first = Digest::MD5.hexdigest("#{@auth[:username]}:#{@digest["realm"]}:#{@auth[:password]}")
+      first = Digest::MD5.hexdigest("#{@config[:username]}:#{@digest["realm"]}:#{@config[:password]}")
       second = Digest::MD5.hexdigest("#{method}:#{request_uri}")
-      cnonce = Digest::MD5.hexdigest("#{@headers["User-Agent"]}:#{@auth[:password]}:#{@request_count}:#{@digest["nonce"]}")
+      cnonce = Digest::MD5.hexdigest("#{@headers["User-Agent"]}:#{@config[:password]}:#{@request_count}:#{@digest["nonce"]}")
 
       if @digest_type.include?("auth")
         hash = Digest::MD5.hexdigest("#{first}:#{@digest["nonce"]}:#{"%08X" % @request_count}:#{cnonce}:#{@digest["qop"]}:#{second}")
@@ -42,7 +49,7 @@ module RETS
         raise RETS::UnsupportedAuth.new("Support for Digest mode #{@digest["qop"]} is not available yet.")
       end
 
-      http_digest = "Digest username=\"#{@auth[:username]}\", "
+      http_digest = "Digest username=\"#{@config[:username]}\", "
       http_digest << "realm=\"#{@digest["realm"]}\", "
       http_digest << "nonce=\"#{@digest["nonce"]}\", "
       http_digest << "uri=\"#{request_uri}\", "
@@ -52,113 +59,116 @@ module RETS
       http_digest << "qop=\"#{@digest["qop"]}\", "
       http_digest << "nc=#{"%08X" % @request_count}, "
       http_digest << "cnonce=\"#{cnonce}\""
-
-      http_digest
     end
 
     ##
     # Creates a HTTP basic header.
     def create_basic
-       "Basic " << ["#{@auth[:username]}:#{@auth[:password]}"].pack("m").delete("\r\n")
+       "Basic " << ["#{@config[:username]}:#{@config[:password]}"].pack("m").delete("\r\n")
     end
 
     ##
-    # Takes a hash and turns it into an escaped query string.
-    def query_string(args)
-      (args.collect {|k, v| "#{k}=#{CGI::escape(v.to_s)}"}).join("&")
-    end
-
-    ##
-    # sends a request to the RETS server.
+    # Sends a request to the RETS server.
+    #
+    # @param [Hash] args
+    # @option args [URI] :url URI to request data from
+    # @option args [Hash, Optional] :params Query string to include with the request
+    # @option args [Integer, Optional] :read_timeout How long to wait for the socket to return data before timing out
+    #
+    # @raise [RETS::ServerError]
+    # @raise [RETS::HTTPError]
+    # @raise [RETS::Unauthorized]
     def request(args, &block)
       request_uri = "#{args[:url].request_uri}"
-      request_uri << "?" << query_string(args[:params]) if args[:params]
-
-      # Increment request count for digest auth
-      @request_count += 1
-
-      # Figure out auth if any
-      args[:headers] ||= {}
-      args[:headers].merge!(@headers)
-
-      if @auth_mode == :digest
-        args[:headers].merge!("Authorization" => create_digest("GET", request_uri))
-      elsif @auth_mode == :basic
-        args[:headers].merge!("Authorization" => create_basic)
-      # From the Interealty implementation at least, RETS-Version is really just an unverified field to "salt" the UA-Auth header.
-      # Will just make up a version and let it do the rest when it's flagged as requiring UA Auth. Might make it auto detect in the future.
-      elsif @auth[:ua_auth] or @auth[:ua_username] or @auth[:ua_password]
-        @auth_mode = :basic
-
-        args[:authing] = true
-        args[:block] = block
-        args[:headers].merge!(
-          "Authorization" => create_basic,
-          "User-Agent" => @auth[:ua_username] || @auth[:username],
-          "RETS-UA-Authorization" => "Digest #{Digest::MD5.hexdigest("#{Digest::MD5.hexdigest("#{@auth[:ua_username] || @auth[:username]}:#{@auth[:ua_password] || @auth[:password]}")}:::1.7")}",
-          "RETS-Version" => "1.7")
+      if args[:params]
+        request_uri << "?"
+        args[:params].each do |k, v|
+          request_uri << "#{k}=#{CGI::escape(v.to_s)}&" if v
+        end
       end
+
+      headers = args[:headers]
+
+      # Digest will change every time due to how its setup
+      @request_count += 1
+      if @auth_mode == :digest
+        headers ||= {}
+        headers.merge!(:Authorization => create_digest("GET", request_uri))
+      end
+
+      headers = headers ? @headers.merge(headers) : @headers
 
       http = ::Net::HTTP.new(args[:url].host, args[:url].port)
       http.read_timeout = args[:read_timeout] if args[:read_timeout]
-
-      resend_request = nil
+      http.set_debug_output(@config[:debug_output]) if @config[:debug_output]
 
       http.start do
-        http.request_get(request_uri, args[:headers]) do |response|
-          # We already authed, and the request became stale so we have to switch to the new auth
+        http.request_get(request_uri, headers) do |response|
+          # Digest can become stale requiring us to reload data
           if @auth_mode == :digest and response.header["www-authenticate"] =~ /stale=true/i
-            mode, header = response.header["www-authenticate"].split(" ", 2)
+            save_digest(response.header["www-authenticate"].split(" ", 2)[1])
+            args[:block] = block
 
-            save_digest(header)
+          elsif response.code == "401"
+            raise RETS::Unauthorized, "Cannot login, check credentials" if @auth_mode
 
-            @headers.delete("Cookie")
-            args[:authing] = true
-            resend_request = true
+            @auth_mode, header = response.header.get_fields("www-authenticate").last.split(" ", 2)
+            @auth_mode = @auth_mode.downcase.to_sym
 
-          # Invalid auth
-          elsif response.code == "401" and !args[:skip_auth]
-            @auth_mode = nil
-
-            # We're already trying to auth, and we still get an invalid auth, can call it a bust and raise
-            raise RETS::InvalidAuth.new("Failed to login") if args[:authing]
-            raise RETS::UnsupportedAuth.new("Unknown authentication method used") unless response.header["www-authenticate"]
-
-            mode, header = response.header["www-authenticate"].split(" ", 2)
-            raise RETS::UnsupportedAuth.new("Unknown HTTP Auth, not digest or basic") unless mode == "Digest" or mode == "Basic"
-
-            args[:authing] = true
-            save_digest(header)
-
-            # Resend the request with the authorization, if it still fails it'll just throw another exception that will bubble up
-            # If the request succeeds, we save that auth mode and will use it on any future requests
-            @auth_mode = mode.downcase.to_sym
-            resend_request = true
-
-          # We just tried to auth, so call the block manually
-          elsif args[:authing]
-            if response.header["set-cookie"]
-              cookies = response.header["set-cookie"].split(",").map do |cookie|
-                cookie.split(";").first.strip
-              end
-
-              @headers.merge!("Cookie" => cookies.join("; "))
+            if @auth_mode == :digest
+              save_digest(header)
+            elsif @auth_mode == :basic
+              @headers.merge!(:Authorization => create_basic)
             end
 
-            args[:block].call(response) if args[:block]
+            # Most RETS implementations don't care about RETS-Version for RETS-UA-Authorization.
+            # Because Rapattoni's does, will set and use it when possible, but otherwise will fake one.
+            # They also seem to require RETS-Version even when it's not required by RETS-UA-Authorization.
+            # Others, such as Offut/Innovia pass the header, but without a version attached.
+            if response.header["rets-version"] and response.header["rets-version"] != ""
+              @headers["RETS-Version"] = response.header["rets-version"]
+            else
+              @headers["RETS-Version"] = "RETS/1.7"
+            end
 
-          # Actual block, call as is
+            if !@headers["RETS-UA-Authorization"] and @headers["RETS-Version"] and @config[:useragent] and @config[:useragent][:password]
+              login = Digest::MD5.hexdigest("#{@config[:useragent][:name]}:#{@config[:useragent][:password]}")
+              @headers.merge!("RETS-UA-Authorization" => "Digest #{Digest::MD5.hexdigest("#{login}:::#{@headers["RETS-Version"]}")}")
+            end
+
+            args[:block] = block
+
+          elsif response.code != "200"
+            if response.body =~ /<RETS/i
+              rets = Nokogiri::XML(response.body).xpath("//RETS")
+              code = rets.attr("ReplyCode").value
+              text = rets.attr("ReplyText").value
+              raise RETS::ServerError.new("#{code}: #{text}", code, text)
+            else
+              raise RETS::HTTPError.new("#{response.code}: #{response.message}", response.code, response.message)
+            end
+
+          # We just tried to auth and don't have access to the original block in yieldable form
+          elsif args[:block]
+            args.delete(:block).call(response)
+
           elsif block_given?
             yield response
+          end
+
+          # Save cookies for session ids and such
+          if response.header["set-cookie"]
+            cookies = response.header.get_fields("set-cookie").map do |cookie|
+              cookie.split(";").first.strip
+            end
+
+            @headers.merge!(:Cookie => cookies.join("; "))
           end
         end
       end
 
-      # Auth failed, resend request
-      if resend_request
-        args[:block] = block
-        self.request(args)
-      end
+      # Something failed, let's try that one more time
+      self.request(args) if args[:block]
     end
   end
 end
