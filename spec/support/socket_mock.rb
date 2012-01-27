1fd549e1979ae4d38233f252d605ba8cca8b84e8
Zachary Anker <zach.anker@gmail.com>
2012-01-26 22:46:50 -0800

    Added a socket mock to easily duplicate the equivalent network socket interface

---
+++ spec/support/socket_mock.rb
@@ -0,0 +1,14 @@
+require "stringio"
+
+module Support
+  module SocketMock
+    def mock_response_with_socket(chunked, size, body)
+      response = mock("Response")
+      response.stub(:content_length).and_return(chunked ? nil : size)
+      response.stub(:chunked?).and_return(chunked)
+      response.stub(:instance_variable_get).with(:@socket).and_return(StringIO.new(body))
+      response.should_receive(:instance_variable_set).at_least(1).with(:@read, true)
+      response
+    end
+  end
+end
